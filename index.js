5b1f7e5b5a182f2902c57cfc003354c91ccb64d2
Philip Olson [merge] <philip.olson@protonmail.ch>
2018-07-26 17:43:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,211 @@
+//------//
+// Init //
+//------//
+
+const areOnlyWhitespace = isOnlyWhitespace,
+  {
+    invalidFirstOrLastLine,
+    invalidSecondLine,
+    invalidFewerThan3Lines,
+    unsupportedArgument,
+  } = getErrorMessages(),
+  onlyWhitespaceRe = /^\s*$/
+
+//
+//------//
+// Main //
+//------//
+
+const tedent = aString => {
+  if (aString === undefined) return ''
+  else if (typeof aString !== 'string') throw new Error(unsupportedArgument)
+
+  // first validate first and last line have only whitespace
+  const allLines = aString.split('\n')
+
+  // handling less than 3 lines requires special treatment
+  if (allLines.length < 3) {
+    if (all(areOnlyWhitespace)(allLines)) return ''
+    else throw new Error(invalidFewerThan3Lines)
+  }
+
+  const firstLine = allLines[0],
+    secondLine = allLines[1],
+    lastLine = allLines[allLines.length - 1]
+
+  if (!all(isOnlyWhitespace)([firstLine, lastLine])) {
+    throw new Error(invalidFirstOrLastLine)
+  } else if (isOnlyWhitespace(secondLine)) {
+    throw new Error(invalidSecondLine)
+  }
+
+  // valid input woo woo !
+
+  const anchor = getAnchor(secondLine)
+
+  // allLines is being mutated for performance
+  return passThrough(allLines, [
+    removeFirstAndLast,
+    indentWith(anchor),
+    trimLastLines,
+    joinWith('\n'),
+  ])
+}
+
+//
+//------------------//
+// Helper Functions //
+//------------------//
+
+function joinWith(separator) {
+  return arrayOfStrings => arrayOfStrings.join(separator)
+}
+
+function isOnlyWhitespace(aString) {
+  return onlyWhitespaceRe.test(aString)
+}
+
+//
+// - removes any tailing lines which only contain whitespace
+// - last line with a non-whitespace character has tailing whitespace removed
+//
+function trimLastLines(arrayOfStrings) {
+  let i = arrayOfStrings.length - 1
+
+  for (; i >= 0; i -= 1) {
+    if (isOnlyWhitespace(arrayOfStrings[i])) {
+      arrayOfStrings.pop()
+    } else {
+      const currentString = arrayOfStrings[i]
+      let j = currentString.length - 1
+      for (; j >= 0; j += 1) {
+        if (j !== ' ') break
+      }
+      arrayOfStrings[i] = currentString.slice(0, j + 1)
+    }
+  }
+
+  return arrayOfStrings
+}
+
+function removeFirstAndLast(allLines) {
+  allLines.pop()
+  allLines.shift()
+  return allLines
+}
+
+function passThrough(argument, arrayOfFunctions) {
+  return arrayOfFunctions.reduce(
+    (result, aFunction) => aFunction(result),
+    argument
+  )
+}
+
+function indentWith(anchor) {
+  return allLines => {
+    if (anchor === 0) return allLines
+
+    for (let i = 0; i < allLines.length; i += 1) {
+      allLines[i] = adjustWhitespace(allLines[i], anchor)
+    }
+
+    return allLines
+  }
+}
+
+function adjustWhitespace(line, anchor) {
+  let i = 0
+  for (; i < line.length; i += 1) {
+    if (line[i] !== ' ') break
+  }
+
+  if (i < anchor) return line
+
+  const leadingSpaces = createStringOfSpaces(i - anchor),
+    restOfLine = line.slice(i)
+
+  return leadingSpaces + restOfLine
+}
+
+function getAnchor(secondLine) {
+  return keepFirstWhile(strictlyEquals(' '))(secondLine).length
+}
+
+function strictlyEquals(left) {
+  return right => left === right
+}
+
+function keepFirstWhile(predicate) {
+  return anArrayLike => {
+    let i = 0
+    for (; i < anArrayLike.length; i += 1) {
+      if (!predicate(anArrayLike[i], i, anArrayLike)) break
+    }
+    return anArrayLike.slice(0, i)
+  }
+}
+
+function all(predicate) {
+  return anArrayLike => {
+    for (let i = 0; i < anArrayLike.length; i += 1) {
+      if (!predicate(anArrayLike[i], i, anArrayLike)) return false
+    }
+    return true
+  }
+}
+
+//
+// implementation thanks to Jon Schlinkert
+//
+/*!
+ * repeat-string <https://github.com/jonschlinkert/repeat-string>
+ *
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+function createStringOfSpaces(desiredLength) {
+  // cover common, quick use cases
+  if (desiredLength === 0) return ''
+  else if (desiredLength === 1) return ' '
+  else if (desiredLength === 2) return '  '
+
+  const max = ' '.length * desiredLength
+  let result = '',
+    spaces = ' '
+
+  while (max > result.length && desiredLength > 1) {
+    if (desiredLength & 1) {
+      result += spaces
+    }
+
+    desiredLength >>= 1
+    spaces += spaces
+  }
+
+  result += spaces
+  result = result.substr(0, max)
+
+  return result
+}
+
+function getErrorMessages() {
+  const expectedUsage =
+    'Expected usage:\ntedent(`\n  Some string\n  ${here}\n`)'
+
+  return {
+    invalidFirstOrLastLine: `The first and last lines are only allowed to contain whitespace\n${expectedUsage}`,
+    invalidSecondLine: `The second line must have a non-whitespace character\n${expectedUsage}`,
+    invalidFewerThan3Lines:
+      'tedent expects a string with three or more lines.  When fewer are' +
+      '\n  passed then they must contain only whitespace for this error not' +
+      `\n  to be thrown.\n${expectedUsage}`,
+    unsupportedArgument: `tedent requires a string or 'undefined' argument\n${expectedUsage}`,
+  }
+}
+//
+//---------//
+// Exports //
+//---------//
+
+module.exports = tedent
---
+++ readme.md
@@ -0,0 +1,152 @@
+# Tedent
+
+Keep your multi-line templated strings lookin' good :sunglasses:
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+**Table of Contents**
+
+- [What is it?](#what-is-it)
+- [What does the name stand for?](#what-does-the-name-stand-for)
+- [Why create it?](#why-create-it)
+- [Simple Usage](#simple-usage)
+- [How the indentation works](#how-the-indentation-works)
+- [Important Usage Notes](#important-usage-notes)
+  - [edge-cases and input requirements](#edge-cases-and-input-requirements)
+- [Test](#test)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+### What is it?
+
+- A function similar to [dedent](https://github.com/dmnd/dedent) just with
+  different semantics
+
+### What does the name stand for?
+
+- `Te`mplate string
+- in`dent`ation
+
+names are hard
+
+### Why create it?
+
+- dedent didn't handle the following case like I wanted
+
+```js
+//
+// any multi-line indented string will do, but stringifying an object is the
+//   common case for me
+//
+const boroughs = ['Brooklyn', 'Manhattan'],
+  boroughsString = JSON.stringify(boroughs, null, 2)
+
+console.log(
+  dedent(`
+    New York boroughs
+    ${boroughs}
+  `)
+)
+
+/*
+expected:
+New York boroughs
+[
+  "Brooklyn",
+  "Manhattan"
+]
+
+actual:
+New York boroughs
+  [
+"Brooklyn",
+"Manhattan"
+]
+*/
+```
+
+### Simple Usage
+
+```js
+import tedent from 'tedent'
+
+console.log(
+  tedent(`
+    This will be indented
+      as you expect
+  `)
+)
+```
+
+### How the indentation works
+
+The indentation scheme is simple. `tedent` takes the second line in the string
+and sets the number of leading spaces as the 'anchor'. All subsequent lines are
+then indented according to the anchor using the rules:
+
+- if the number of leading spaces is greater or equal to the anchor, then
+  indent `numLeadingSpaces - anchor`
+- if it's less, then indent `numLeadingSpaces`
+
+Keep in mind the above rules are catered to multiline variables. The result will
+look weird if you manually expand the variables
+
+i.e. this is expected
+
+```js
+const moreInfo = 'more\ninfo'
+tedent(`
+    Some string
+    ${moreInfo}
+`)
+```
+
+not this
+
+```js
+tedent(`
+    Some string
+    more
+info
+`)
+```
+
+`tedent` will handle both the same obviously, but the first one reads
+as intended
+
+### Important Usage Notes
+
+- First of all, this library doesn't handle tabs. I will accept a PR
+  with support
+
+- Secondly, if you always use `tedent` like the following
+
+  ```js
+  tedent(`
+    at least one line
+  `)
+  ```
+
+  then you shouldn't run into any issues. However we all know input can be
+  tricky so `tedent` has a few edge-cases built-in as well as input requirements
+
+#### edge-cases and input requirements
+
+- if the first argument is anything but `undefined` or `typeof 'string'` then an error will be thrown
+- if you pass `undefined` an empty string is returned
+- if you pass a string with three or more lines, then
+  - the first and last lines must contain only whitespace
+  - the second line must contain a non-whitespace character
+  - _an error will be thrown if the above two conditions are not met_
+- if you pass a string with fewer than 3 lines
+  - if they only contain whitespace then an empty string is returned
+  - otherwise an error is thrown
+- finally, all trailing whitespace from the result is trimmed
+
+I didn't feel it necessary to explain the reasons for my choices in handling
+edge-cases, but if you have questions please ask via github issues.
+
+### Test
+
+`./run test`
